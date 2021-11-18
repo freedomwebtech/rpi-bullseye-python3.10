37459bbaf7953495bcd4227a88add4ce92c3307b
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-11-18 13:26:22 +0530

    Update install3.10.sh

--- install3.10.sh
+++ install3.10.sh
@@ -1,5 +1,5 @@
 sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev 
-sudo wgethttps://www.python.org/ftp/python/3.10.0/Python-3.10.0.tar.xz
+sudo wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tar.xz
 sudo tar -xvf Python-3.10.0.tar.xz
 cd Python-3.10.0
 sudo ./configure --enable-optimizations

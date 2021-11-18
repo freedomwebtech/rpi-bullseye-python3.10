2c101f11b5e5718ddbc2746c05cd96e640c8f170
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-11-18 12:43:56 +0530

    Create install3.10.sh

---
+++ install3.10.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev 
+sudo wgethttps://www.python.org/ftp/python/3.10.0/Python-3.10.0.tar.xz
+sudo tar -xvf Python-3.10.0.tar.xz
+cd Python-3.10.0
+sudo ./configure --enable-optimizations
+sudo make -j 4
+sudo make altinstall
